cee9c1b8b7e360ba2ffcf011ced0f356ebdabb9b
Aitchx <72714897+Aitchx@users.noreply.github.com>
2021-10-18 22:27:25 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -472,6 +472,8 @@ class Gambling(commands.Cog):
                     x = clean_list.index(str(ctx.author))
                     string_list[x + 1].strip()
                     if (total - total_c) > 2:
+                        string_list[x + 1] = str(int(string_list[x + 1]) + int(bet * 2.3))
+                    elif (total - total_c) > 4:
                         string_list[x + 1] = str(int(string_list[x + 1]) + int(bet * 3))
                     else:
                         string_list[x + 1] = str(int(string_list[x + 1]) + (bet * 2))
